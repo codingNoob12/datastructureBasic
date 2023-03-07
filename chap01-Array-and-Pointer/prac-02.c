218eadbf3b254985e67bb994d869cdb1c8818150
codingNoob12 <crm26@naver.com>
2023-03-07 15:19:17 +0900

    chap01-prac03

--- chap01-Array-and-Pointer/prac-02.c
+++ chap01-Array-and-Pointer/prac-02.c
@@ -3,9 +3,14 @@
 int main(void) {
     int n;
     
-    printf("\n1~9의 정수를 입력하세요 : ");
-    scanf("%d", &n);
-    printf("\n");
+    while (1) {
+        printf("1~9의 정수를 입력하세요 : ");
+        scanf("%d", &n);
+        if (n >= 1 && n <= 9) {
+            printf("\n");
+            break;
+        }
+    }
 
     for (int i = 1; i <= 9; i++) {
         printf("%d * %d = %d\n", n, i, n * i);
