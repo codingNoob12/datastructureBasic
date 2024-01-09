8108ba086ae313c38bbb7136f05ed253c7a8cf24
codingNoob12 <ilhyeon0326@gmail.com>
2024-01-10 08:38:00 +0900

    added gitignore

--- chap01-Array-and-Pointer/prac-03.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    char str[] = "Data Structure!";
-
-    printf("문자 배열 str[] : %s\n", str);
-
-    return 0;
-}
--- test/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    char a[20];
-    scanf("%s", a);
-    printf("%s\n", a);
-    return 0;
-}
